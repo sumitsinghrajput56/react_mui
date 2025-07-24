324efe7f212dddbd1d1194969b975cecc51b485c
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-07-25 00:05:43 +0530

    added FavoriteBorder and Favorite

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { pink } from "@mui/material/colors";
 import Checkbox from "@mui/material/Checkbox";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
@@ -18,7 +19,7 @@ export default function ColorCheckboxes() {
 
   return (
     <div>
-      {/* <Checkbox {...label} defaultChecked /> */}
+      <Checkbox {...label} defaultChecked  icon={<FavoriteBorder/>} checkedIcon={<Favorite/>} />
       <Checkbox {...label} defaultChecked color="secondary" value="firstCheckbox" onChange={(e)=>setValue(e)} />
       <Checkbox {...label} defaultChecked color="success" value="secondCheckbox" onChange={(e)=>setValue(e)} />
       <Checkbox {...label} defaultChecked color="default" value="thiredCheckbox" onChange={(e)=>setValue(e)} indeterminate />
