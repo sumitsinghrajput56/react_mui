6fe23ce616e4273b10e4782bc92d3574eadd1a57
sumitsinghrajput56 <sumitsinghrajput56@gmail.com>
2025-07-25 01:11:44 +0530

    made slider

--- src/App.js
+++ src/App.js
@@ -2,28 +2,45 @@ import * as React from "react";
 import { pink } from "@mui/material/colors";
 import Checkbox from "@mui/material/Checkbox";
 import { Favorite, FavoriteBorder } from "@mui/icons-material";
-import { Radio } from "@mui/material";
+import { Radio, Slider } from "@mui/material";
 
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function ColorCheckboxes() {
+  const [gender, setGender] = React.useState("male");
 
-  const [gender,setGender]=React.useState("male");
+  const mark = [
+    {
+      value: 0,
+      label: "start",
+    },
+    {
+      value: 200,
+      label: "end",
+    },
+    {
+      value: 100,
+      label: "mid",
+    },
+  ];
 
- const setValue=(e)=>{
-   console.log(gender);
-   setGender(e.target.value);
- }
+  const getValue=(e)=>
+  {
+    console.log(e.target.value);
+  }
 
   return (
     <div>
-      <div>
-        <Radio color="secondary"  value="male" checked={gender==="male"} onChange={setValue}/>
-        <span>male</span>
-      </div>
-       <div>
-        <Radio color="primary" value="female" checked={gender==="female"}  onChange={setValue} />
-        <span>female</span>
+      <div style={{ width: 100, marginLeft: 100 ,marginTop:50}}>
+        <Slider 
+        max={200} 
+        defaultValue={70} 
+        marks={mark} 
+        step={20}
+        valueLabelDisplay="auto"
+        onChange={getValue}
+        color="secondary"
+        />
       </div>
     </div>
   );
